39bd5d16eea200c90f52badf31c36eee6217b1df
Edgar Arroyo <Edgar@iMac-de-Edgar.local>
2016-10-14 01:28:25 -0500

    HOTFIX: ROUTES nows work

---
+++ app/Http/routes.php
@@ -0,0 +1,28 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+$app->get('/',function(){
+  return view('login');
+});
+$app->get('wosFiles',function(){
+  return view('wosfiles');
+});
+$app->post('/createNewFolder','filesController@createNewFolder');
+$app->post('/getMyFolders','filesController@getMyFolders');
+$app->get('/carpeta={id}','filesController@gotoFolder');
+$app->post('/createNewFile','filesController@createNewFile');
+$app->post('/getFilesByFolder','filesController@getFilesByFolder');
+$app->post('/login','userController@login');
+
+
+$app->get('desktop',function(){
+  return view('desktop');
+});
